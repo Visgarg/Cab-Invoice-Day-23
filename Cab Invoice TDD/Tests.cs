7aad981ace3c4f554691bcd842aac0a009f6af6f
Vishal Garg <vishugarg10004@gmail.com>
2020-11-01 10:40:47 +0530

    [Vishal] welcome message

---
+++ Cab Invoice TDD/Tests.cs
@@ -0,0 +1,34 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Tests.cs" company="Capgemini">
+//   Copyright
+// </copyright>
+// <creator Name="Vishal Garg"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Cab_Invoice_TDD
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// adding class to do tests for program
+    /// </summary>
+    public class Tests
+    {
+        /// <summary>
+        /// Setups this instance.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Test1s this instance.
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ CabInvoiceDay23/Program.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Capgemini">
+//   Copyright
+// </copyright>
+// <creator Name="Vishal Garg"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace CabInvoiceDay23
+{
+    using System;
+
+    /// <summary>
+    /// program class which calls main method
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Program");
+        }
+    }
+}
